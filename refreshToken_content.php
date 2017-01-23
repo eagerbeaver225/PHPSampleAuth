827ae1b2fed3ab4ad49343bb4508473cc54dd2a5
Oliver Fiala <oliverfiala@hotmail.com>
2017-01-23 11:56:36 +0100

    add echo in refresh token page

--- refreshToken_content.php
+++ refreshToken_content.php
@@ -12,8 +12,8 @@
         <h1>Refresh Access Token</h1>
         <div class="panel panel-primary">
           <div class="panel-heading">Status:</div>
-          <div class="panel-body"> <?php $status ?>
-            <pre><?php $tokenValue ?></pre>
+          <div class="panel-body"> <?php echo $status ?>
+            <pre><?php echo $tokenValue ?></pre>
           </div>
         </div>
     <a href="index.php" class="btn btn-primary">Back</a> <a href="refreshToken.php" class="btn btn-primary">Retry token refresh</a>
