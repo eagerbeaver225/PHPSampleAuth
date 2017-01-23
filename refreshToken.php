58a8c0c9a9521a699c18482f3f5b5a0042557296
Oliver Fiala <oliverfiala@hotmail.com>
2017-01-23 16:27:58 +0100

    retry token refresh

--- refreshToken.php
+++ refreshToken.php
@@ -6,10 +6,11 @@ $token = array();
 AppCache::init();
 session_start();
 $token = $_SESSION['token'];
-$token = array_filter($token);
-if(empty($token)){
+
+if(empty($token) || !isset($token)){    
     header("index.php");
     die();
+    return;
 }
 
 global $status, $tokenVaule;
@@ -19,9 +20,11 @@ if(strlen($refreshedToken["refresh_token"])){
     AppCache::$AccessToken = $refreshedToken["refresh_token"];
     $tokenValue = $refreshedToken["refresh_token"];
     $status = "Token refreshed";
+    $_SESSION['token'] = $refreshedToken;
 }else{
     $tokenValue = "null";
     $status = "Token refresh failed";
+    unset($_SESSION['token']);
 }
 
 include('refreshToken_content.php');
